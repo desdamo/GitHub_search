79dbfa411a03d8041486575fb92fd9f6342151bb
desdamo <desdamo@gmail.com>
2020-12-19 18:20:34 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+
+
+
+import App from 'src/components/App';
+
+
+
+
+const rootReactElement = <App />;
+
+const target = document.getElementById('root');
+
+render(rootReactElement, target);
